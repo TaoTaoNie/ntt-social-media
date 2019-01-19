ff78cf95d4ce7d3539a454be0bedb605d00a74fd
leetHuam <980321356@qq.com>
2019-01-19 14:28:12 +0800

    修改了一些list

--- src/main/java/com/ntt/nttsocialmedia/repositories/EmployeeRepository.java
+++ src/main/java/com/ntt/nttsocialmedia/repositories/EmployeeRepository.java
@@ -4,7 +4,10 @@ import com.ntt.nttsocialmedia.beans.Employee;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+import java.util.ArrayList;
+
 public interface EmployeeRepository extends ReactiveCrudRepository<Employee, Long>, ReactiveQueryByExampleExecutor {
 //    Flux<Employee> findByFirstName(Mono<String> name);
 //    ArrayList<Integer> arrayList = new ArrayList<>();
+    ArrayList<Integer> list = new ArrayList<>();
 }
